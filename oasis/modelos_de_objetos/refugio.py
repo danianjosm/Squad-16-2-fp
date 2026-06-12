b04072028f738f354a5f7b65e8e8e3c39d735308
danianjosm <dani.a.menezes@outlook.com>
2026-06-12 18:42:05 -0300

    docs: adiciona docstrings em RefugioObj e UsuarioObj

--- oasis/modelos_de_objetos/refugio.py
+++ oasis/modelos_de_objetos/refugio.py
@@ -1,4 +1,13 @@
 class RefugioObj:
+    """
+    Representa um ponto de refugio climático
+    atributos 
+        nome(str): nome do refugio
+        endereço(str): Localização no formato UF, CIDADE, BAIRRO
+        status(str):Ativo ou fechado 
+        latitude(float): codernada geográfica
+        longitude(float): Coordenada geográfica  
+    """
     def __init__(self, nome, endereco, status, recursos, latitude, longitude):
         self.nome = nome.strip()
         self.endereco = endereco.strip()
@@ -9,3 +18,4 @@ class RefugioObj:
 
     def formatar_para_linha(self):
         return f"| {self.nome} | {self.endereco} | {self.status} | {self.recursos} | {self.latitude} | {self.longitude} |"
+    """Converte o objeto em uma linha formatada para salvar no banco de dados """
